b000400041b15d7b2e4efe501bd957d70c50e7e6
ffmaer <ffmaer@gmail.com>
2013-09-24 15:40:13 -0400

    add project

--- Typer/ViewController.m
+++ Typer/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *selectedText;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UIStepper *stepper;
 
 @end
 
@@ -18,6 +21,13 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self select];
+    
+}
+
+-(NSArray *)wordList{
+    NSArray *wordList = [[self.label.attributedText string] componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    return wordList;
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +35,49 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)select {
+    self.stepper.maximumValue = [[self wordList] count]-1;
+    self.selectedText.text = [self selectedWord];
+}
+
+-(NSString *)selectedWord{
+    return [[self wordList] objectAtIndex:self.stepper.value];
+}
+
+-(void)changeAttribute:(NSDictionary *)attribute{
+    NSMutableAttributedString *mat = [self.label.attributedText mutableCopy];
+    [mat addAttributes:attribute range:[[self.label.attributedText string] rangeOfString:[self selectedWord]]];
+    self.label.attributedText = mat;
+}
+
+- (IBAction)changeColor:(UIButton *)sender {
+    [self changeAttribute:@{NSForegroundColorAttributeName : sender.backgroundColor}];
+}
+
+
+- (IBAction)changeFont:(UIButton *)sender {
+    CGFloat fontSize = [UIFont systemFontSize];
+    NSDictionary *attributes = [self.label.attributedText attributesAtIndex:0 effectiveRange:NULL];
+    UIFont *existingFont = attributes[NSFontAttributeName];
+    if(existingFont) fontSize = existingFont.pointSize;
+    
+    UIFont *font = [sender.titleLabel.font fontWithSize:fontSize];
+    [self changeAttribute:@{NSFontAttributeName : font}];
+}
+- (IBAction)underline:(UIButton *)sender {
+    [self changeAttribute:@{NSUnderlineStyleAttributeName : @(NSUnderlineStyleSingle)}];
+}
+- (IBAction)ununderline:(UIButton *)sender {
+        [self changeAttribute:@{NSUnderlineStyleAttributeName : @(NSUnderlineStyleNone)}];
+}
+- (IBAction)outline:(UIButton *)sender {
+    [self changeAttribute:@{NSStrokeWidthAttributeName : @1}];
+}
+- (IBAction)unoutline {
+        [self changeAttribute:@{NSStrokeWidthAttributeName : @0}];
+}
+
+
 
 @end
